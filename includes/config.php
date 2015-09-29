817f5863a58aab30c5fa0a167dd5482e06c978e5
numericOverflow <github_com@bytality.com>
2015-09-29 15:06:43 -0500

    Update config.php
    Tweaks to support auto-update scored via curl/wget in cron script

--- includes/config.php
+++ includes/config.php
@@ -19,5 +19,16 @@ define('SEASON_YEAR', '2015');
 //set timezone offset, hours difference between your server's timezone and eastern
 define('SERVER_TIMEZONE_OFFSET', 0);
 
+//define a batch update "key" that a cronjob can pass to update scores automatically
+//can be anything you want, as long as it can be sent as a get parameter on the URL
+//NOTE: make your life easier and just use alpha-numerics w/out any special chars...
+//NOTE: THE PAGE NAME IS CASE SENSEITVE TO BYPASS LOG-IN, IF THE CASE NOT MATCH, IT WILL REDIRECT TO LOGIN W/OUT UPDATING SCORES!
+//example:
+// curl -O 'http://www.yourdomain.com/getHtmlScores.php?BATCH_SCORE_UPDATE_KEY=yourRandomDefinedValueHere'
+// wget 'http://www.yourdomain.com/getHtmlScores.php?BATCH_SCORE_UPDATE_KEY=yourRandomDefinedValueHere'
+define('BATCH_SCORE_UPDATE_KEY', 'f5fe57a1950f903a420e6c43cc266f62');
+//enable or disable batch updates here
+define('BATCH_SCORE_UPDATE_ENABLED', true);
+
 // ***DO NOT EDIT ANYTHING BELOW THIS LINE***
 error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);

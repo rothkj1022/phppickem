e778a46811cb57e7d96ca59bdd295a6d2799e7d3
root <root@msgbyte.stvin.net>
2016-09-11 09:15:43 -0600

    moved sections into separate files

--- includes/column_right.php
+++ includes/column_right.php
@@ -1,98 +1,15 @@
-		<p class="skip2content"><a href="<?php echo $_SERVER['REQUEST_URI']; ?>#content">Skip to content &raquo;</a></p>
-
-		<div class="bg-primary">
-			<b>Current Time (<?php echo SERVER_TIMEZONE_ABBR; ?>):</b> <?php echo date('Y_m_d'); ?>
-			<span id="jclock1"></span>
-			<script type="text/javascript">
-			$(function($) {
-				var optionsEST = {
-			        timeNotation: '24h',
-			        am_pm: false,
-					utc: true,
-					utc_offset: <?php echo -1 * (4 + SERVER_TIMEZONE_OFFSET); ?>
-				}
-				$('#jclock1').jclock(optionsEST);
-		    });
-			</script>
-		</div>
-
-		<!-- start countdown code - http://keith-wood.name/countdown.html -->
-		<?php
-		if ($firstGameTime !== $cutoffDateTime && !$firstGameExpired) {
-		?>
-		<div id="firstGame" class="countdown bg-success"></div>
-		<script type="text/javascript">
-		//set up countdown for first game
-		var firstGameTime = new Date("<?php echo date('F j, Y H:i:00', strtotime($firstGameTime)); ?>");
-		firstGameTime.setHours(firstGameTime.getHours() -1);
-		$('#firstGame').countdown({until: firstGameTime, description: 'until first game is locked'});
-		</script>
-		<?php
-		}
-		if (!$weekExpired) {
-		?>
-		<div id="picksLocked" class="countdown bg-danger"></div>
-		<script type="text/javascript">
-		//set up countdown for picks lock time
-		var picksLockedTime = new Date("<?php echo date('F j, Y H:i:00', strtotime($cutoffDateTime)); ?>");
-		picksLockedTime.setHours(picksLockedTime.getHours() -1);
-		$('#picksLocked').countdown({until: picksLockedTime, description: 'until week <?php echo $currentWeek; ?> is locked'});
-		</script>
-		<?php
-		} else {
-			//current week is expired
-		}
-		?>
-		<!-- end countdown code -->
-
 <?php
-$weekStats = array();
-$playerTotals = array();
-$possibleScoreTotal = 0;
-calculateStats();
 
-$tmpWins = 0;
-$i = 1;
-if (is_array($playerTotals) && sizeof($playerTotals) > 0) {
-	//show top 3 winners
-	echo '		<div class="bg-success">' . "\n";
-	echo '			<b>Current Leaders (# wins):</b><br />' . "\n";
-	arsort($playerTotals);
-	foreach($playerTotals as $playerID => $stats) {
-		if ($tmpWins < $stats[wins]) $tmpWins = $stats[wins]; //set initial number of wins
-		//if next lowest # of wins is reached, increase counter
-		if ($stats[wins] < $tmpWins ) $i++;
-		//if wins is zero or counter is 3 or higher, break
-		if ($stats[wins] == 0 || $i > 3) break;
-		echo '			' . $i . '. ' . $stats[name] . ' - ' . $stats[wins] . (($stats[wins] > 1) ? ' wins' : ' win') . '<br />';
-		$tmpWins = $stats[wins]; //set last # wins
-	}
-	echo '		</div>' . "\n";
-}
+// dtjr
+include('includes/column_countdown.php');
 
-$tmpScore = 0;
-$i = 1;
-if (is_array($playerTotals) && sizeof($playerTotals) > 0) {
-	//show top 3 pick ratios
-	echo '		<div class="bg-success">' . "\n";
-	echo '			<b>Current Leaders (pick %):</b><br />' . "\n";
-	$playerTotals = sort2d($playerTotals, 'score', 'desc');
-	foreach($playerTotals as $playerID => $stats) {
-		if ($tmpScore < $stats[score]) $tmpScore = $stats[score]; //set initial top score
-		//if next lowest score is reached, increase counter
-		if ($stats[score] < $tmpScore ) $i++;
-		//if score is zero or counter is 3 or higher, break
-		if ($stats[score] == 0 || $i > 3) break;
-		$pickRatio = $stats[score] . '/' . $possibleScoreTotal;
-		$pickPercentage = number_format((($stats[score] / $possibleScoreTotal) * 100), 2) . '%';
-		echo '			' . $i . '. ' . $stats[name] . ' - ' . $pickRatio . ' (' . $pickPercentage . ')<br />';
-		$tmpScore = $stats[score]; //set last # wins
-	}
-	echo '		</div>' . "\n";
-}
+include('includes/column_stats.php');
+// dtjr
 
 if (COMMENTS_SYSTEM !== 'disabled') {
 	echo '		<p class="bg-info"><b>Taunt your friends!</b><br /><a href="'.$_SERVER['REQUEST_URI'].'#comments">Post a comment</a> now!</p>'."\n";
 }
 
 //include('includes/comments.php');
+
+?>

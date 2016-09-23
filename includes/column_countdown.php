cbd1be8475f29bd656f1a0fbe0cffdf6d74ec4c3
root <root@msgbyte.stvin.net>
2016-09-22 20:49:16 -0600

    fixes to get firstGameTime to work accross subsequent weeks with correct cutoff date/time

--- includes/column_countdown.php
+++ includes/column_countdown.php
@@ -18,6 +18,18 @@
 
 <!-- start countdown code - http://keith-wood.name/countdown.html -->
 <?php
+$week = (int)$_GET['week'];
+if (empty($week)) {
+	//get current week
+	$week = (int)getCurrentWeek();
+}
+
+// echo "week = |" . $week . "|\n";
+$firstGameTime = getFirstGameTime($week);
+$firstGameExpired = ((date("U", time()+(SERVER_TIMEZONE_OFFSET * 3600)) > strtotime($firstGameTime)) ? true : false);
+// echo "firstGameTime = |" . $firstGameTime . "|\n";
+// echo "cutoffDateTime = |" . $cutoffDateTime . "|\n";
+// echo "firstGameExpired = |" . $firstGameExpired . "|\n";
 if ($firstGameTime !== $cutoffDateTime && !$firstGameExpired) {
 ?>
 <div id="firstGame" class="countdown bg-success"></div>
@@ -26,17 +38,17 @@ if ($firstGameTime !== $cutoffDateTime && !$firstGameExpired) {
 var firstGameTime = new Date("<?php echo date('F j, Y H:i:00', strtotime($firstGameTime)); ?>");
 // firstGameTime.setHours(firstGameTime.getHours() -1);
 firstGameTime.setHours(firstGameTime.getHours() - <?php echo SERVER_TIMEZONE_OFFSET; ?>);
-$('#firstGame').countdown({until: firstGameTime, description: 'until first game is locked'});
+$('#firstGame').countdown({until: firstGameTime, description: 'until first game in week <?php echo $week; ?> is locked'});
 </script>
 <?php
 }
 if (!$weekExpired) {
 
-$week = (int)$_GET['week'];
-if (empty($week)) {
-	//get current week
-	$week = (int)getCurrentWeek();
-}
+// $week = (int)$_GET['week'];
+// if (empty($week)) {
+// 	//get current week
+// 	$week = (int)getCurrentWeek();
+// }
 
 ?>
 <div id="picksLocked" class="countdown bg-danger"></div>
@@ -46,7 +58,7 @@ var picksLockedTime = new Date("<?php echo date('F j, Y H:i:00', strtotime($cuto
 // picksLockedTime.setHours(picksLockedTime.getHours() -1);
 picksLockedTime.setHours(picksLockedTime.getHours() - <?php echo SERVER_TIMEZONE_OFFSET; ?>);
 // $('#picksLocked').countdown({until: picksLockedTime, description: 'until week <?php echo $currentWeek; ?> is locked'});
-$('#picksLocked').countdown({until: picksLockedTime, description: 'until week <?php echo $week; ?> is locked'});
+$('#picksLocked').countdown({until: picksLockedTime, description: 'until all games in week <?php echo $week; ?> is locked'});
 </script>
 <?php
 } else {
